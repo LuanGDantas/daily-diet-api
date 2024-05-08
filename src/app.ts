055e42178a0af77c589c28610027165bf0ab1ead
LuanGDantas <luan.dantas123@hotmail.com>
2024-05-08 10:51:33 -0300

    refactor: rename user route files

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
-import { usersRoutes } from './routes/user.routes'
+import { usersRoutes } from './routes/users.routes'
 
 export const app = fastify()
 
